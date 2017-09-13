2814b194198b0f1102482f86cacfbf9cadb9e98b
soni28 <soni28@ccs.neu.edu>
2017-09-13 19:41:54 -0400

    Added github and heroku links on home page

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -169,7 +169,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/components/home/home.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "\n<div class=\"container\">\n\n  <h1>\n    Angular 4 MEAN stack app\n  </h1>\n\n\n  <h3>\n    App works\n  </h3>\n\n\n  <h4>\n    <a href=\"test\">Test MongoDB</a>\n  </h4>\n\n</div>\n\n"
+module.exports = "\n<div class=\"container\">\n\n  <h1>\n    Angular 4 MEAN stack app\n  </h1>\n\n\n  <h3>\n    App works\n  </h3>\n\n\n  <h4>\n    <a href=\"test\">Test MongoDB</a>\n  </h4>\n\n  <h5>\n    <a href=\"https://github.com/ssoni28/webdev-soni-swati\">GitHub URL</a>\n  </h5>\n\n  <h6>\n    <a href=\"https://webdev-soni-swati.herokuapp.com/\">Heroku</a>\n  </h6>\n</div>\n\n"
 
 /***/ }),
 
