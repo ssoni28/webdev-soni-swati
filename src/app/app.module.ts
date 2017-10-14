038381c76dea65f2669eced42c7033a032ed6511
soni28 <soni28@ccs.neu.edu>
2017-10-14 18:36:31 -0400

    Created folders in app components and created components for respective features as given in step 1 and 2 in assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,45 @@ import {Routing} from './app.routing';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {TestService} from './services/test.service.client';
+import {LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
+import { WebsiteListComponent } from './components/website/website-list/website-list.component';
+import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
+import { PageEditComponent } from './components/page/page-edit/page-edit.component';
+import { PageListComponent } from './components/page/page-list/page-list.component';
+import { PageNewComponent } from './components/page/page-new/page-new.component';
+import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
+import { WidgetHeadingComponent } from './components/widget/widget-heading/widget-heading.component';
+import { WidgetImageComponent } from './components/widget/widget-image/widget-image.component';
+import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
+import { WidgetYoutubeComponent } from './components/widget/widget-youtube/widget-youtube.component';
+import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
+import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-header/widget-header.component';
 
 @NgModule({
   // Declare components here
   declarations: [
     AppComponent,
     HomeComponent,
-    TestComponent
+    TestComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    WebsiteEditComponent,
+    WebsiteListComponent,
+    WebsiteNewComponent,
+    PageEditComponent,
+    PageListComponent,
+    PageNewComponent,
+    WidgetChooserComponent,
+    WidgetHeadingComponent,
+    WidgetImageComponent,
+    WidgetListComponent,
+    WidgetYoutubeComponent,
+    WidgetEditComponent,
+    WidgetHeaderComponent
   ],
   imports: [
     BrowserModule,
